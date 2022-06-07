c25d7d28613126f4183947039eb3dc0a3f292579
ygh65 <ygh65@192.168.10.120>
2022-06-07 20:13:49 +0900

    c4->switchCase 수정

--- src/chapter4/SwitchCase.java
+++ src/chapter4/SwitchCase.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class SwitchCase {
 
 	public static void main(String[] args) {
-		int ranking = 4;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("몇 등? >>");
+		int ranking = sc.nextInt();
 		char medalColor;
 		switch (ranking) {
 		case 1:
@@ -18,23 +20,22 @@ public class SwitchCase {
 			medalColor = '동';
 			break;
 		default:
-			medalColor = '녹';
+			medalColor = '?';
 
 		}
-		System.out.println(ranking + "등 메달의 색깔은 " + medalColor + "색입니다");
+		System.out.println(ranking + "등 메달의 색깔은 " + medalColor + "색입니다! ㅊㅋㅊㅋ");
 
 		System.out.println("----------------");
-		Scanner sc = new Scanner(System.in);
-		System.out.println("ㅇ월?");
+		System.out.println("ㅇ월? >>");
 		int month = sc.nextInt();
 		int day;
 
 		switch (month) {
-		case 1, 3, 5, 7, 8, 10, 12 :
+		case 1, 3, 5, 7, 8, 10, 12:
 			day = 31;
 			System.out.println(day);
 			break;
-			
+
 		case 4, 6, 9, 11:
 			day = 30;
 			System.out.println(day);
@@ -43,6 +44,8 @@ public class SwitchCase {
 			day = 28;
 			System.out.println(day);
 			break;
+		default:
+			System.out.println("잘못 입력하셨습니다... 1~12 중 하나를 입력해주삼");
 		}
 	}
 
