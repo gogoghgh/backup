c23832f6c7538df107f8ecfcaeb28886eb4e4aaf
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2022-06-07 14:12:21 +0900

    되나?

--- src/p06_02/Review1.java
+++ src/p06_02/Review1.java
@@ -1,12 +1,14 @@
-//package p06_02;
-//
-//public class Review1 {
-//	public static void main(String[] args) {
-//		boolean run = true;
-//		int balance = 0;
-//		Scanner scanner = new Scanner(System.in);
-//		
-//		while(run)
-//	}
-//
-//}
+package p06_02;
+
+import java.util.Scanner;
+
+public class Review1 {
+	public static void main(String[] args) {
+		boolean run = true;
+		int balance = 0;
+		Scanner scanner = new Scanner(System.in);
+		
+		
+	}
+
+}
