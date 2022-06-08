1d43b4e5ee642b19106eebd353685a785433a7bb
ITWILL <ITWILL@DESKTOP-NC7RQTD>
2022-06-08 18:08:23 +0900

    한글 깨지나??? 제발~~~~

--- src/chapter4/SwitchCase.java
+++ src/chapter4/SwitchCase.java
@@ -6,47 +6,22 @@ public class SwitchCase {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int ranking;
 		System.out.println("몇 등? >>");
-		int ranking = sc.nextInt();
+		ranking = sc.nextInt();
 		char medalColor;
-		switch (ranking) {
-		case 1:
-			medalColor = '금';
-			break;
-		case 2:
-			medalColor = '은';
-			break;
-		case 3:
-			medalColor = '동';
-			break;
-		default:
-			medalColor = '?';
-
+		
+		switch(ranking) {
+		case 1: medalColor = '금';
+		break;
+		case 2: medalColor = '은';
+		break;
+		case 3: medalColor = '동';
+		break;
+		default: medalColor = 'ㅠ';
 		}
-		System.out.println(ranking + "등 메달의 색깔은 " + medalColor + "색입니다! ㅊㅋㅊㅋ");
-
-		System.out.println("----------------");
-		System.out.println("ㅇ월? >>");
-		int month = sc.nextInt();
-		int day;
-
-		switch (month) {
-		case 1, 3, 5, 7, 8, 10, 12:
-			day = 31;
-			System.out.println(day);
-			break;
-
-		case 4, 6, 9, 11:
-			day = 30;
-			System.out.println(day);
-			break;
-		case 2:
-			day = 28;
-			System.out.println(day);
-			break;
-		default:
-			System.out.println("잘못 입력하셨습니다... 1~12 중 하나를 입력해주삼");
-		}
+		
+		System.out.println(ranking + "등 메달의 색깔은 " + medalColor +"색입니다. ㅊㅋㅊㅋ");
 	}
 
 }
