8e6c39efec07bf3d6dd1813f7f4b0f15fac8b9c8
ITWILL <ITWILL@DESKTOP-NC7RQTD>
2022-06-09 17:52:31 +0900

    06_09 쉅 끝 커밋 완

--- src/p06_09/Star.java
+++ src/p06_09/Star.java
@@ -1,19 +1,84 @@
 package p06_09;
 
+// º° Âï±â ¼÷Á¦..
 public class Star {
 
 	public static void main(String[] args) {
-		for (int a = 1; a <= 4; a++) {
-			System.out.print(" ");
-			for (int b = 1; b<=3; b++) {
-				System.out.print(" ");
-				for (int c = 1; c<=2; c++) {
-					System.out.print(" ");
-					for (int d = 1; d<=1; d++) {
-						System.out.print("*");
-					}System.out.println();
-				}
-			}
+		int a, b, c, d, e, f, g;
+		for (a = 1; a <= 1; a++) {
+			System.out.print("*");
 		}
+		System.out.println();
+		for (a = 1; a <= 2; a++) {
+			System.out.print("*");
+		}
+		System.out.println();
+		for (a = 1; a <= 3; a++) {
+			System.out.print("*");
+		}
+		System.out.println();
+		for (a = 1; a <= 4; a++) {
+			System.out.print("*");
+		}
+		System.out.println();
+		System.out.println("---------------");
+		for(a=1; a<=4; a++) {
+			System.out.print("*");
+			for(b=1; b<=4; b++) {
+				if(b!=1) {
+				System.out.print("*");
+			} else System.out.print(" ");
+		}
+		
+
+//		for (int a = 1; a <= 4; a++) {
+//			System.out.print(" ");
+//			for (int b = 1; b<=3; b++) {
+//				System.out.print(" ");
+//				for (int c = 1; c<=2; c++) {
+//					System.out.print(" ");
+//					for (int d = 1; d<=1; d++) {
+//						System.out.print("*");
+//					}System.out.println();
+//					
+		System.out.println();
+		System.out.println("--------------------");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.println();
+
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print(" ");
+		System.out.print(" ");
+		System.out.println();
+		
+		System.out.print(" ");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print(" ");
+		System.out.println();
+		
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println();
+
 	}
+}
 }
