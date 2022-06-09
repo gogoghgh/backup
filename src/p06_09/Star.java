37314c8d90f445b986bd9f58b4629c810f1ef0f1
ygh65 <ygh65@192.168.10.120>
2022-06-09 23:27:12 +0900

    별 찍기 완

--- src/p06_09/Star.java
+++ src/p06_09/Star.java
@@ -4,81 +4,67 @@ package p06_09;
 public class Star {
 
 	public static void main(String[] args) {
-		int a, b, c, d, e, f, g;
-		for (a = 1; a <= 1; a++) {
-			System.out.print("*");
-		}
-		System.out.println();
-		for (a = 1; a <= 2; a++) {
-			System.out.print("*");
-		}
-		System.out.println();
-		for (a = 1; a <= 3; a++) {
-			System.out.print("*");
-		}
-		System.out.println();
-		for (a = 1; a <= 4; a++) {
-			System.out.print("*");
-		}
-		System.out.println();
-		System.out.println("---------------");
-		for(a=1; a<=4; a++) {
-			System.out.print("*");
-			for(b=1; b<=4; b++) {
-				if(b!=1) {
+		System.out.println("--------------직사각형");
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
 				System.out.print("*");
-			} else System.out.print(" ");
+			}
+			System.out.println();
 		}
-		
 
-//		for (int a = 1; a <= 4; a++) {
-//			System.out.print(" ");
-//			for (int b = 1; b<=3; b++) {
-//				System.out.print(" ");
-//				for (int c = 1; c<=2; c++) {
-//					System.out.print(" ");
-//					for (int d = 1; d<=1; d++) {
-//						System.out.print("*");
-//					}System.out.println();
-//					
-		System.out.println();
-		System.out.println("--------------------");
-		System.out.print(" ");
-		System.out.print(" ");
-		System.out.print(" ");
-		System.out.print("*");
-		System.out.print(" ");
-		System.out.print(" ");
-		System.out.print(" ");
-		System.out.println();
+		System.out.println("-----------직각삼각형");
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 
-		System.out.print(" ");
-		System.out.print(" ");
-		System.out.print("*");
-		System.out.print("*");
-		System.out.print("*");
-		System.out.print(" ");
-		System.out.print(" ");
-		System.out.println();
-		
-		System.out.print(" ");
-		System.out.print("*");
-		System.out.print("*");
-		System.out.print("*");
-		System.out.print("*");
-		System.out.print("*");
-		System.out.print(" ");
-		System.out.println();
-		
-		System.out.print("*");
-		System.out.print("*");
-		System.out.print("*");
-		System.out.print("*");
-		System.out.print("*");
-		System.out.print("*");
-		System.out.print("*");
-		System.out.println();
+		System.out.println("------------직각삼각형(역)");
+		for (int i = 0; i < 5; i++) {
+//			for (int j = 5; i < j; j--) { 쪼까 아쉬운 가현스 답^^;;ㅎㅎㅎ
+			for (int j = 0; j < 5 - i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		System.out.println("------------피라미드(가현)"); // ㅁㅊㅁㅊ대박 나왔다!!!!!!! 이게 되네 ㄷㄷ
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 7; j++) {
+				if ((i + j < 3) || (j - i > 3)) {
+					System.out.print(" ");
+				} else
+					System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 19; j++) {
+				if ((i + j < 9) || (j - i > 9)) {
+					System.out.print(" ");
+				} else
+					System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		System.out.println("------------피라미드(구글)");
+		for (int i = 1; i < 6; i++) {
+			for (int j = 5; j > i; j--) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i * 2 - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		System.out.println("-----------피라미드(역)");
+//		for (int i = 0; i < 4; i++) {
+//			for (int j=0; j < )
+//	}
 
 	}
-}
 }
