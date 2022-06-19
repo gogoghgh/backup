79501d2a198f6573d8256942e8b6990ffe53b523
gogoghgh <gogoghgh@yugahyeonuimbp>
2022-06-20 00:57:34 +0900

    숙제 좀 해봄

--- src/p06_14/MyDate.java
+++ src/p06_14/MyDate.java
@@ -1,9 +1,15 @@
 package p06_14;
 
+import java.util.Scanner;
+
 public class MyDate {
 	// 생성자
-	public MyDate(int day, int month, int year) {
-		super();
+
+	Scanner sc = new Scanner(System.in);
+	
+	public MyDate() {}
+	
+	public MyDate(int year, int month, int day) {
 		this.day = day;
 		this.month = month;
 		this.year = year;
@@ -15,8 +21,12 @@ public class MyDate {
 	private int year;
 	// private int day, month, year;
 
+	
 	// 메서드덜
-	public void setDay(int day) {
+	public void setDay() {
+		System.out.println("day 입력하삼");
+		int day = sc.nextInt(); 
+//		여기다가 할 필요 없었네~~ 걍 메인에서 생성자 만들 때 매개변수로 nextInt하니까 걍 
 		if (month == 2) {
 			if (day < 1 || day > 28) {
 				System.out.println("오류입니다(day: 1~28까지 입력해주삼)");
@@ -28,7 +38,10 @@ public class MyDate {
 
 	}
 
-	public void setMonth(int month) {
+	public void setMonth() {
+		System.out.println("month 입력하삼");
+		int month = sc.nextInt();
+		
 		if (month < 1 || month > 12) {
 			System.out.println("오류입니다(month: 1~12까지 입력해주삼)");
 		} else {
@@ -36,7 +49,10 @@ public class MyDate {
 		}
 	}
 
-	public void setYear(int year) {
+	public void setYear() {
+		System.out.println("year 입력하삼");
+		int year = sc.nextInt();
+		
 		if (year < 0) {
 			System.out.println("오류입니다");
 		} else {
@@ -58,14 +74,33 @@ public class MyDate {
 
 	// 더 자세히 하려면 103쪽 보면서!!!
 	public boolean isValid() {
-		if (month == 2) {
+		switch (month) {
+		case 1, 3, 5, 7, 8, 10, 12:
+			if (day < 1 || day > 31) {
+				return false;
+			} else
+				return true;
+		case 2:
 			if (day < 1 || day > 28) {
 				return false;
-			} // 2월에~ day가 1보다 작고 28보다 크면(=비정상)? false를 리턴
-		} // 정상이면?? 빠져나와서,,
+			}
+			return true;
+		case 4, 6, 9, 11:
+			if (day < 1 || day > 30) {
+				return false;
+			} else
+				return true;
+		default: return false;
+		}
 
-		return true; // true를 리턴
-	}
+//		if (month == 2) {
+//			if (day < 1 || day > 28) {
+//				return false;
+//			} // 2월에~ day가 1보다 작고 28보다 크면(=비정상)? false를 리턴
+//		} // 정상이면?? 빠져나와서,,
+//
+//		return true; // true를 리턴
+//	}
 
 //		내 답^^;;
 //		return (day <1 || day>28? )? System.out.println("유효하지 않은 날짜입니다") : System.out.println("유효한 날짜입니다")
@@ -77,4 +112,5 @@ public class MyDate {
 //	case 2:
 //	case 4, 6, 9, 11: 	}
 
+	}
 }
--- src/p06_14/MyDateTest.java
+++ src/p06_14/MyDateTest.java
@@ -1,21 +1,33 @@
 package p06_14;
 
+import java.util.Scanner;
+
 public class MyDateTest {
 
 	public static void main(String[] args) {
-		MyDate date1 = new MyDate(30, 2, 2000);
+		System.out.println("year, month, day 순서대로 입력>>");
+		Scanner sc = new Scanner(System.in);
+		MyDate date1 = new MyDate();
+		
+		
+		date1.setYear();
+		date1.setMonth();
+		date1.setDay();
+		
+		System.out.println();
 		System.out.print(date1.getYear() + "년  " + date1.getMonth() + "월  " + date1.getDay() + "일");
-		if (date1.isValid() == true) {
-			System.out.println("  유효한 날짜입니다");
-		} else
-			System.out.println("  유효하지 않은 날짜입니다");
-
-		MyDate date2 = new MyDate(2, 10, 2006);
-		System.out.println(date2.isValid());
+		System.out.println();
 		if (date1.isValid() == true) {
 			System.out.println("유효한 날짜입니다");
 		} else
 			System.out.println("유효하지 않은 날짜입니다");
+
+//		MyDate date2 = new MyDate(2, 10, 2006);
+//		System.out.println(date2.isValid());
+//		if (date1.isValid() == true) {
+//			System.out.println("유효한 날짜입니다");
+//		} else
+//			System.out.println("유효하지 않은 날짜입니다");
 
 //		date.setYear(2023);
 //		date.setMonth(2);
